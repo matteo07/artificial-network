05905e30d63a8e428e5a1f16b6c6a3d16e2d944b
MrDema <matteodema17@gmail.com>
2017-04-28 09:30:45 +0200

    added guide

--- ai.sh
+++ ai.sh
@@ -2,67 +2,96 @@
 
 import scala.collection.mutable.ArrayBuffer
 
-def debug = true
+//Inizializza i Weight
+val w_in1 = 1
+val w_in2 = 1
+val w_in3 = 1
 
-def test(cond: Boolean,message: String) = {
-	if (debug) assert(cond,message)
-}
+val w_mid1 = 0.5
+val w_mid2 = 1
 
-class Neuron(val in: ArrayBuffer[Int], val weight: Int){
-	def this() = this(new ArrayBuffer[Int](), 1)
+val w_out1 = 1
+val w_out2 = 0.5
+val w_out3 = 1
+
+println("started...")
+
+class Neuron(val in: ArrayBuffer[Double], val weight: Double){
+	def this() = this(new ArrayBuffer[Double](), 1)
 	
-	def getIn(): Int = {
-		val res = 1
-		for(i <- in) {
-			println(""+i)
+	//ritorna la media dei valori di In
+	def getIn(): Double = {
+		var res = 0.0
+		for(i <- 0 until in.length) {
+			res += in(i)
 		}
-		return res
+		return res / in.length
 	}
 	
 	def compute() = getIn * weight
-	
+		
 	def isEmpty() = true
 
 	override def toString = "A neuron"
 }
 
-def computeIn(neurons: ArrayBuffer[Neuron]): ArrayBuffer[Int] = {
-	val res = new ArrayBuffer[Int]()
-	for(i <- 1 to neurons.length) {
+//restituisce l'array di in data un array di neuron
+def computeIn(neurons: ArrayBuffer[Neuron]): ArrayBuffer[Double] = {
+	val res = new ArrayBuffer[Double]()
+	for(i <- 0 until neurons.length) {
 		res += neurons(i).compute
 	}
 	return res
 }
 
 class NeuralNetwork(){
-	val input = new ArrayBuffer[Int]
+	val input = new ArrayBuffer[Double]
 	input += 1
 	
 	val in = new ArrayBuffer[Neuron]
-	val middle = new ArrayBuffer[Neuron]
+	val mid = new ArrayBuffer[Neuron]
 	val out = new ArrayBuffer[Neuron]
 	
-	in += new Neuron(input,1)
-	in += new Neuron(input,1)
-	in += new Neuron(input,1)
+	in += new Neuron(input,w_in1)
+	in += new Neuron(input,w_in2)
+	in += new Neuron(input,w_in3)
 	
+	mid += new Neuron(computeIn(in),w_mid1)
+	mid += new Neuron(computeIn(in),w_mid2)
+	
+	out += new Neuron(computeIn(mid),w_out1)
+	out += new Neuron(computeIn(mid),w_out2)
+	out += new Neuron(computeIn(mid),w_out3) 	
+
+	println("Computed result :: " + computeIn(out))
+}
+
+new NeuralNetwork()
+
+//============================  TESTS  ===============================
+
+def debug = true
+
+def test(cond: Boolean,message: String) = {
+	if (debug) assert(cond,message)
+}
+
+def testComputeIn() = {
+	val expects = new ArrayBuffer[Double]
+	val input = new ArrayBuffer[Double]
+	input += (1,0,2)
+	expects += (1,1,1)
+	val in = new ArrayBuffer[Neuron]
 	
 	for( a <- 1 to 3){
 		in += new Neuron(input,1)
 	}
 	
-	
-	
-	val out1 = new Neuron()
-	val out2 = new Neuron()
-	val out3 = new Neuron()
-	
-	out += new Neuron()
-	out += new Neuron()
-	out += new Neuron()
+	assert(computeIn(in)==expects)	
 }
 
-new NeuralNetwork()
-
+testComputeIn()
 test((true),"Doh")
+
+
 println("GVNG")
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env scala
+
+
+import sys.process._
+
+"./ai.sh" !
+
+
